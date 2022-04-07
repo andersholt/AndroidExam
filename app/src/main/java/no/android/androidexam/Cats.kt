302c3fdb9b15bc11466a8b95ba23e9565836b9ab
Ellaoui94 <abab010@student.kristiania.no>
2022-04-07 10:22:24 +0200

    Fragment 2 is working 💯

---
+++ app/src/main/java/no/android/androidexam/Cats.kt
@@ -0,0 +1,6 @@
+package no.android.androidexam
+
+data class Cats(
+    val name : String,
+    val picture: Int
+)
--- app/src/main/java/no/android/androidexam/ImageAdapter.kt
+++ app/src/main/java/no/android/androidexam/ImageAdapter.kt
@@ -1,42 +1,70 @@
 package no.android.androidexam
 
 import android.graphics.Color
+import android.text.Layout
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.GridView
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
-import androidx.core.view.get
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-class ImageAdapter : RecyclerView.Adapter<ImageAdapter.ItemViewHolder>() {
 
-class ItemViewHolder(private val view:View) : RecyclerView.ViewHolder(view)
+class ImageAdapter( val cats: List<Cats>) : RecyclerView.Adapter<ImageAdapter.ItemViewHolder>() {
+
+    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        var image: ImageView
+
+        init {
+            image = view.findViewById(R.id.image)
+        }
+    }
+
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-        val liner1 = LinearLayout(parent.context)
-        liner1.layoutParams = LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT, 0.7f)
 
-        val nameView: TextView = TextView(parent.context)
-        nameView.setPadding(10, 0, 10, 0)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.imagerow, parent, false)
+       /* val ll1: LinearLayout = LinearLayout(parent.context)
+        ll1.layoutParams = LinearLayout.LayoutParams(
+            0,
+            LinearLayout.LayoutParams.WRAP_CONTENT, 0.7f)
 
         val imageView: ImageView = ImageView(parent.context)
-        imageView.setPadding(10,0,10,0)
+        imageView.layoutParams = ViewGroup.LayoutParams(400, 400)
+        imageView.setBackgroundColor(Color.BLACK)
+        imageView.setPadding(50,0,20,0)
 
-        liner1.addView(imageView)
-        liner1.addView(nameView)
+        ll1.addView(imageView)
 
-        return ItemViewHolder(liner1)
+        val ll3: LinearLayout = LinearLayout(parent.context)
+        ll3.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+        )
+
+        ll3.weightSum = 1.0f
+
+
+        ll3.addView(ll1)*/
+
+        return ItemViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-        holder.itemView.tag = position
-        (((holder.itemView as LinearLayout).getChildAt(0) as LinearLayout).getChildAt(0) as TextView).text =
-            "Mjau"
-        ((holder.itemView.getChildAt(0) as LinearLayout).getChildAt(1) as ImageView).setBackgroundColor(Color.BLACK)
+        val studentInfo: Cats = cats[position]
+
+        holder.image.setImageResource(studentInfo.picture)
+
+/*
+        (((holder.itemView as LinearLayout).getChildAt(0) as LinearLayout).getChildAt(0) as ImageView).setImageDrawable(holder.itemView.context.getDrawable(studentInfo.picture))
+*/
 
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return 10
     }
 }
